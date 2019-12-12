125d5eecc8475534eda714f280359bab3bc0c40b
K <kittipat_1413@hotmail.com>
2019-12-12 13:01:00 +0700

    ADD PM2.5 Sensor (Dev)

---
+++ PM2.5-Sensor_UART /main.c
@@ -0,0 +1,214 @@
+#include "stm32f10x.h"
+#include "stm32f10x_conf.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void init_usart1(void);
+void send_byte(uint8_t b);
+void usart_puts(char* s);
+void init_usart2(void);
+void send_byte2(uint8_t b);
+void usart_puts2(char* s);
+
+void USART1_IRQHandler(void);
+
+void BUF_Print(uint8_t* data);
+
+uint8_t usart_buff[100] = {'\0'};
+uint8_t data_buff[50] = {'\0'};
+
+uint8_t i = 0;
+
+static inline void Delay_1us(uint32_t nCnt_1us)
+{
+  volatile uint32_t nCnt;
+
+  for (; nCnt_1us != 0; nCnt_1us--)
+    for (nCnt = 13; nCnt != 0; nCnt--);
+}
+
+static inline void Delay(uint32_t nCnt_1us)
+{
+
+			while(nCnt_1us--);
+}
+
+void USART1_IRQHandler(void)
+{
+    uint8_t b;
+    if(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == SET) {
+
+          b =  USART_ReceiveData(USART1);
+          // ---Print HEX--- //
+          // send_byte2(b);
+
+          data_buff[i++] = b;
+          if(i >= 49){
+          	i = 0;
+          	BUF_Print(data_buff);
+
+          }
+          
+
+	}
+}
+
+void send_byte(uint8_t b)
+{
+	/* Send one byte */
+	USART_SendData(USART1, b);
+
+	/* Loop until USART2 DR register is empty */
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+}
+
+
+void usart_puts(char* s)
+{
+    while(*s) {
+    	send_byte(*s);
+        s++;
+    }
+}
+
+
+void send_byte2(uint8_t b)
+{
+  /* Send one byte */
+  USART_SendData(USART2, b);
+
+  /* Loop until USART2 DR register is empty */
+  while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+}
+
+
+void usart_puts2(char* s)
+{
+    while(*s) {
+      send_byte2(*s);
+        s++;
+    }
+}
+
+void init_usart1()
+{
+
+	USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Enable peripheral clocks. */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	/* Configure USART1 Rx pin as floating input. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART1 Tx as alternate function push-pull. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure the USART1 */
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	/* Enable transmit and receive interrupts for the USART1. */
+	USART_ITConfig(USART1, USART_IT_TXE, DISABLE);
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+	USART_Cmd(USART1, ENABLE);
+
+}
+
+void init_usart2(void)
+{
+
+	USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Enable peripheral clocks. */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	/* Configure USART1 Rx pin as floating input. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART1 Tx as alternate function push-pull. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure the USART2 */
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx;
+	USART_Init(USART2, &USART_InitStructure);
+
+	/* Enable transmit and receive interrupts for the USART2. */
+	USART_ITConfig(USART2, USART_IT_TXE, DISABLE);
+	USART_ITConfig(USART2, USART_IT_RXNE,DISABLE);
+
+	USART_Cmd(USART2, ENABLE);
+}
+
+void BUF_Print(uint8_t* data)
+{
+ 	int i;
+    for(i=0;i<=49;i++) {
+        sprintf((char*)usart_buff,"%02X  ", data[i]);
+        usart_puts2(usart_buff); 
+  }
+  usart_puts2("\nEND \n");
+
+}
+
+
+int main(void)
+{
+	// LED Pin
+	GPIO_InitTypeDef GPIO_InitStructure;
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	//Configure LED Pin
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = 	GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = 	GPIO_Pin_13; 	
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	
+
+	init_usart1();
+	init_usart2();
+
+	usart_puts2("Init OK \n");
+
+	while (1) {
+
+			}
+
+		
+	
+}
+
+// 
