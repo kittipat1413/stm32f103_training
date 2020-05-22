e56e3e5059e47681866cb87d33d4a5e3d5af3503
K <kittipat_1413@hotmail.com>
2020-05-22 14:35:58 +0700

    add pin

--- GPIO/main.c
+++ GPIO/main.c
@@ -30,6 +30,17 @@ int main(void)
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	//Configure LED Pin
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = 	GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = 	GPIO_Pin_4 |GPIO_Pin_5 |GPIO_Pin_6 |GPIO_Pin_7; 	
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	GPIO_SetBits(GPIOA,GPIO_Pin_4);
+	GPIO_Write(GPIOA, 240);
+
 	while (1) {
 
 		GPIO_SetBits(GPIOC,GPIO_Pin_13);
